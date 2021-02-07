fcbff9cacb79910003c76478d62abf197ae12251
ice_lfernandes <ice_lfernandes@uolinc.com>
2021-02-07 10:37:58 -0300

    Adição de verbos Http finalizando a API

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,14 +2,36 @@ const Atendimento = require('../models/atendimentos')
 
 /* Export da rota GET */
 module.exports = app => {
+    
     // GET
     app.get('/atendimentos', (req, res) => {
-        
+        Atendimento.lista(res);
+    })
+
+    // GET por ID
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(res, id);
+    })
+
+    // PATCH
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+    
+        Atendimento.altera(id, valores, res)
     })
 
     // POST
     app.post('/atendimentos', (req, res) => {
         const atendimentoRequestBody = req.body
         Atendimento.adiciona(atendimentoRequestBody, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
     })
 }
